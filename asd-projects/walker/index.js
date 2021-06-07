fadad4ea34dda0a054d8a7800d240c789d9d9483
GOFGOLDEN <sidneyselassie1@gmail.com>
2021-06-07 17:13:31 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,5 +1,4 @@
 /* global $, sessionStorage */
-
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
   
 function runProgram(){
@@ -11,7 +10,7 @@ function runProgram(){
   // sidney: this is for the framerate of our program
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  // sidney: this array marks our key's for the controlls of our player
+  // sidney: this is for the controlls of our player
   var KEY = {
     "W":87,
     "A":65,
@@ -19,16 +18,15 @@ function runProgram(){
     "D":68,
   } 
   // Game Item Objects
-  //sidney: this will hold out position and speed for our player. 
-var positionX = 0;
-var positionY = 0;
-var speedX = 0;
-var speedY = 0;
+  var positionX = 0;
+  var positionY = 0;
+  var speedX = 0;
+  var speedY = 0;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keyDown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);      
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -38,45 +36,60 @@ var speedY = 0;
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    repositionGameItem(),  
+    redrawGameItem()
   }
   
   /* 
   Called in response to events.
   */
- // Sidney: This takes our array KEY and uses it to identify key's that we press
-  function handleKeyDown(event) {
-  if (event.which ===KEY.W) {
-    console.log("W")
-    speedY = 5
-  }
-  if (event.which ===KEY.A) {
-    speedX = -5;
-    console.log("A")
-  }
-  if (event.which ===KEY.S) {
-    console.log("S")
-    speedY -5
-  }
-  if (event.which ===KEY.D) {
-    console.log("D")
-    speedX = 5
-  }
-  }
+    function handleKeyDown(event) {
+      if (event.which ===KEY.W) {
+        console.log("W")
+        speedY = -5;
+      }
+      if (event.which ===KEY.A) {
+        speedX = -5;
+        console.log("A")
+      }
+      if (event.which ===KEY.S) {
+        console.log("S")
+        speedY = 5;
+      }
+      if (event.which ===KEY.D) {
+        console.log("D")
+        speedX = 5;
+      }
+  }function handleKeyUp(event) {
+    if (event.which ===KEY.W) {
+      console.log("W")
+      speedY = 0;
+    }
+    if (event.which ===KEY.A) {
+      speedX = 0;
+      console.log("A")
+    }
+    if (event.which ===KEY.S) {
+      console.log("S")
+      speedY = 0;
+    }
+    if (event.which ===KEY.D) {
+      console.log("D")
+      speedX = 0;
+    }
+}
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-//sidney: this tells the program that movment of the player is dependent on the positioning
   function repositionGameItem(){
     positionX += speedX
     positionY += speedY
   }
   //Sidney: this tells the css to redraw the box based off of the position.
   function redrawGameItem(){
-    $("#box").css("left",positionX);
-    $("#box").css("top",positionY);
+    $("#gameItem").css("left",positionX);
+    $("#gameItem").css("top",positionY);
     
   }
   function endGame() {

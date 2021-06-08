8baab26adcc8e1bb760876cd39dcadf5bb6eaee6
GOFGOLDEN <sidneyselassie1@gmail.com>
2021-06-08 18:18:08 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -9,24 +9,34 @@ function runProgram(){
   // Constant Variables
   // sidney: this is for the framerate of our program
   var FRAME_RATE = 60;
-  var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  var FRAMES_PER_SECOND_INTERVAL = 500 / FRAME_RATE;
   // sidney: this is for the controlls of our player
   var KEY = {
     "W":87,
     "A":65,
     "S":83,
     "D":68,
+    "UP":38,
+    "RIGHT":37,
+    "DOWN":40,
+    "LEFT":39,
   } 
   // Game Item Objects
   var positionX = 0;
   var positionY = 0;
   var speedX = 0;
   var speedY = 0;
+  var positionX2 = 0;
+  var positionY2 = 0;
+  var speedX2 = 0;
+  var speedY2 = 0;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);      
+  $(document).on('keydown', handleKeyDown2);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp2);   
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -38,46 +48,88 @@ function runProgram(){
   function newFrame() {
     repositionGameItem(),  
     redrawGameItem()
+      repositionGameItem2(),  
+      redrawGameItem2()
+    
   }
   
   /* 
   Called in response to events.
-  */
-    function handleKeyDown(event) {
-      if (event.which ===KEY.W) {
-        console.log("W")
-        speedY = -5;
-      }
-      if (event.which ===KEY.A) {
-        speedX = -5;
-        console.log("A")
-      }
-      if (event.which ===KEY.S) {
-        console.log("S")
-        speedY = 5;
-      }
-      if (event.which ===KEY.D) {
-        console.log("D")
-        speedX = 5;
-      }
-  }function handleKeyUp(event) {
+  *///This handleKeyDown function uses our KEY object 
+  //to corispond position x and y using our wasd keys
+  function handleKeyDown(event) {
     if (event.which ===KEY.W) {
       console.log("W")
-      speedY = 0;
+      speedY = -5;
     }
     if (event.which ===KEY.A) {
-      speedX = 0;
+      speedX = -5;
       console.log("A")
     }
     if (event.which ===KEY.S) {
       console.log("S")
-      speedY = 0;
+      speedY = 5;
     }
     if (event.which ===KEY.D) {
       console.log("D")
-      speedX = 0;
+      speedX = 5;
     }
-}
+    /// this handleKeyUp function stops the position of x or y when we let go of our wasd keys
+}function handleKeyUp(event) {
+  if (event.which ===KEY.W) {
+    console.log("W")
+    speedY = 0;
+  }
+  if (event.which ===KEY.A) {
+    speedX = 0;
+    console.log("A")
+  }
+  if (event.which ===KEY.S) {
+    console.log("S")
+    speedY = 0;
+  }
+  if (event.which ===KEY.D) {
+    console.log("D")
+    speedX = 0;
+  }
+}//This handleKeyDown function uses our KEY object 
+  //to corispond position x and y using our wasd keys
+  function handleKeyDown2(event) {
+    if (event.which ===KEY.UP) {
+      console.log("UP")
+      speedY2 = -5;
+    }
+    if (event.which ===KEY.RIGHT) {
+      speedX2 = -5;
+      console.log("RIGHT")
+    }
+    if (event.which ===KEY.DOWN) {
+      console.log("DOWN")
+      speedY2 = 5;
+    }
+    if (event.which ===KEY.LEFT) {
+      console.log("LEFT")
+      speedX2 = 5;
+    }
+    /// this handleKeyUp function stops the position of x or y when we let go of our wasd keys
+  }
+  function handleKeyUp2(event) {
+    if (event.which ===KEY.UP) {
+      console.log("UP")
+      speedY2 = 0;
+    }
+    if (event.which ===KEY.RIGHT) {
+      speedX2 = 0;
+      console.log("RIGHT")
+    }
+    if (event.which ===KEY.DOWN) {
+      console.log("DOWN")
+      speedY2 = 0;
+    }
+    if (event.which ===KEY.LEFT) {
+      console.log("LEFT")
+      speedX2 = 0;
+    }}
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -86,11 +138,18 @@ function runProgram(){
     positionX += speedX
     positionY += speedY
   }
+  function repositionGameItem2(){
+    positionX2 += speedX2
+    positionY2 += speedY2
+  }
   //Sidney: this tells the css to redraw the box based off of the position.
   function redrawGameItem(){
     $("#gameItem").css("left",positionX);
     $("#gameItem").css("top",positionY);
-    
+  }
+  function redrawGameItem2() {
+    $("#gameItem2").css("left",positionX2);
+    $("#gameItem2").css("top",positionY2);
   }
   function endGame() {
     // stop the interval timer

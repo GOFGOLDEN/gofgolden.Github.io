391a91255cf43b9de32d3cef74bc096b2726be24
GOFGOLDEN <sidneyselassie1@gmail.com>
2021-06-10 19:42:23 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -6,28 +6,93 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+  //this will hold out key inputs that we will use to move the players
+  var KEY = {
+    "W":87,
+    "A":65,
+    "S":83,
+    "D":68,
+    "UP":38,
+    "RIGHT":37,
+    "DOWN":40,
+    "LEFT":39,
+  } 
+  //this is our position and speed variables. we will use this to communicate to other functions to make movment in the players
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
+  var positionX = 0;
+  var positionY = 0;
+  var speedX = 0;
+  var speedY = 0;
+  var positionX2 = 0;
+  var positionY2 = 0;
+  var speedX2 = 0;
+  var speedY2 = 0;
 
 
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)                        // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);      
+  $(document).on('keydown', handleKeyDown2);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp2);   
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  function handleKeyDown(event) {
+    if (event.which ===KEY.W) {
+      console.log("W")
+      speedY = -5;
+    }
+    if (event.which ===KEY.S) {
+      console.log("S")
+      speedY = 5;
+    }
+    /// this handleKeyUp function stops the position of x or y when we let go of our wasd keys
+}function handleKeyUp(event) {
+  if (event.which ===KEY.W) {
+    console.log("W")
+    speedY = 0;
+  }
+  if (event.which ===KEY.S) {
+    console.log("S")
+    speedY = 0;
+  }
+}//This handleKeyDown function uses our KEY object 
+  //to corispond position x and y using our wasd keys
+  function handleKeyDown2(event) {
+    if (event.which ===KEY.UP) {
+      console.log("UP")
+      speedY2 = -5;
+    }
+    if (event.which ===KEY.DOWN) {
+      console.log("DOWN")
+      speedY2 = 5;
+    }
+    /// this handleKeyUp function stops the position of x or y when we let go of our wasd keys
+  }
+  function handleKeyUp2(event) {
+    if (event.which ===KEY.UP) {
+      console.log("UP")
+      speedY2 = 0;
+    }
+    if (event.which ===KEY.DOWN) {
+      console.log("DOWN")
+      speedY2 = 0;
+    }}
 
   /* 
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem(),  
+    redrawGameItem()
+      repositionGameItem2(),  
+      redrawGameItem2()
 
   }
   
@@ -42,7 +107,23 @@ function runProgram(){
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function repositionGameItem(){
+    positionX += speedX
+    positionY += speedY
+  }
+  function repositionGameItem2(){
+    positionX2 += speedX2
+    positionY2 += speedY2
+  }
+  //Sidney: this tells the css to redraw the box based off of the position.
+  function redrawGameItem(){
+    $("#gameItem").css("left",positionX);
+    $("#gameItem").css("top",positionY);
+  }
+  function redrawGameItem2() {
+    $("#gameItem2").css("left",positionX2);
+    $("#gameItem2").css("top",positionY2);
+  }
   function endGame() {
     // stop the interval timer
     clearInterval(interval);

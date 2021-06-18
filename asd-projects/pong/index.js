1287ae447faba8b78e65b3ca6b6993ba189cb6f0
GOFGOLDEN <sidneyselassie1@gmail.com>
2021-06-18 02:38:56 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -18,6 +18,7 @@ function runProgram(){
     "DOWN":40,
     "LEFT":39,
   } 
+  //this defines our board width and hight as well as the ball size. this will be used to identify when the ball bounces
   var BOARD_WIDTH = 1500;
   var BOARD_HEIGHT = 800;
   var BALL_SIZE = 40; 
@@ -40,6 +41,7 @@ function runProgram(){
   var paddle1 = player('#gameItem');
   var ball = player('#ball');
   // one-time setup
+  // this variable will be used to update new frames as well as keep our new frame function continuesly working
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)                        // change 'eventType' to the type of event you want to handle
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);      
@@ -90,16 +92,17 @@ function runProgram(){
       paddle2.speedY = 0;
     }
 }
+// this moveball function takes the css of the ball and the speed (x/y) to redraw the ball based on its position/speed
 function moveBall(ball) {
 // repositioning
-  ball.x += ball.velocityX;
-  ball.y += ball.velocityY;
+  ball.x += ball.speedX;
+  ball.y += ball.speedY;
 
 // redrawing
   $(ball.id).css("left", ball.x);
   $(ball.id).css("top", ball.y);
 }
-
+// like it is said this function detects the bounce of the ball and returns the speed in the other direction
 function detectBounce(ball) {
   if (ball.x > BALL_MAX.RIGHT) {
       ball.x = BALL_MAX.RIGHT;
@@ -134,11 +137,12 @@ function detectBounce(ball) {
   Called in response to events.
   */
   
-//Okay, the problem is that you named your variables "paddle1" and "paddle2", but throughout most of your program you are referring to "player". The only place you should have "player" is in your "factoryPlayer" function. Finally, you have "return players" instead of "return player" in that function.
+
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+  // this is our factory function that we use to create player 1 and 2 (as well as the ball)
   function player (id) {
     var player = {};
     player.id = id;
@@ -151,6 +155,7 @@ function detectBounce(ball) {
     console.log(player);
     return player;
   }
+  //these update the position of both player one and two as well as the ball. 
   function repositionGameItem(){
     paddle1.Y += paddle1.speedY
   }
@@ -161,7 +166,7 @@ function detectBounce(ball) {
     ball.X += ball.speedX;
     ball.Y += ball.speedY;
   }
-  //Sidney: this tells the css to redraw the box based off of the position.
+  
   function redrawGameItem(){
     $(paddle1.id).css("left",paddle1.X);
     $(paddle1.id).css("top",paddle1.Y);

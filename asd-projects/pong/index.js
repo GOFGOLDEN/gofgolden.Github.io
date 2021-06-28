bf6d72d158dbdc3ae975441a8caa48ecc31376f2
GOFGOLDEN <sidneyselassie1@gmail.com>
2021-06-28 19:16:36 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -19,21 +19,25 @@ function runProgram(){
     "LEFT":39,
   } 
   //this defines our board width and hight as well as the ball size. this will be used to identify when the ball bounces
+ var PLAYER_WIDTH = 25;    
+ var PLAYER_HEIGHT = 250;
   var BOARD_WIDTH = 1500;
   var BOARD_HEIGHT = 800;
   var BALL_SIZE = 40; 
-  var R
   var BALL_MAX = {
     "LEFT": 0,
     "RIGHT": BOARD_WIDTH - BALL_SIZE,
     "TOP": 0,
     "BOTTOM": BOARD_HEIGHT - BALL_SIZE
 }
-
-function RANDOM_SPEED() {
-  R = Math.random()
+var PLAYER_MAX = {
+  "LEFT": 0,
+  "RIGHT": BOARD_WIDTH  - PLAYER_WIDTH,
+  "TOP": 0,
+  "BOTTOM": BOARD_HEIGHT - PLAYER_HEIGHT
 }
-RANDOM_SPEED()
+
+
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
@@ -43,7 +47,8 @@ RANDOM_SPEED()
   var paddle2 = player('#gameItem2');
   var paddle1 = player('#gameItem');
   var ball = player('#ball');
-  RANDOM_BOUNCE()
+  ball.speedX = 7;
+  ball.speedY = 7;
   // one-time setup
   // this variable will be used to update new frames as well as keep our new frame function continuesly working
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)                        // change 'eventType' to the type of event you want to handle
@@ -80,14 +85,14 @@ RANDOM_SPEED()
   function handleKeyDown2(event) {
     if (event.which ===KEY.UP) {
       console.log("UP")
-      paddle2.speedY = -7;
+      paddle2.speedY = -4;
     }
     if (event.which ===KEY.DOWN) {
       console.log("DOWN")
-      paddle2.speedY = 7;
+      paddle2.speedY = 4;
     }
     /// this handleKeyUp function stops the position of x or y when we let go of our wasd keys
-  }
+}
   function handleKeyUp2(event) {
     if (event.which ===KEY.UP) {
       console.log("UP")
@@ -98,18 +103,7 @@ RANDOM_SPEED()
       paddle2.speedY = 0;
     }
 }
-function RANDOM_BOUNCE(){
-  if (R > 0.5){
-    ball.speedX = 5;
-    ball.speedY = 5;
-    console.log('1')
-  }
-  else {
-    ball.speedY = -5;
-    ball.speedY = -5;
-    console.log('2')
-  }
-}
+
 // this moveball function takes the css of the ball and the speed (x/y) to redraw the ball based on its position/speed
 
 function moveBall(ball) {
@@ -122,7 +116,7 @@ function moveBall(ball) {
   $(ball.id).css("top", ball.Y);
 }
 // like it is said this function detects the bounce of the ball and returns the speed in the other direction
-function detectBounce(ball,paddle1,paddle2) {
+function detectWallBounce(ball) {
   if (ball.X > BALL_MAX.RIGHT) {
       ball.X = BALL_MAX.RIGHT;
       ball.speedX *= -1;
@@ -134,12 +128,66 @@ function detectBounce(ball,paddle1,paddle2) {
   else if (ball.Y < BALL_MAX.TOP) {
       ball.Y = BALL_MAX.TOP;
       ball.speedY *= -1;
+     
   }
   else if (ball.Y > BALL_MAX.BOTTOM) {
     ball.Y = BALL_MAX.BOTTOM;
     ball.speedY *= -1;
+  
+  }
+  else if (paddle1.Y < PLAYER_MAX.TOP) {
+    paddle1.Y = PLAYER_MAX.TOP;
+    paddle1.speedY = 0;
+  }
+  else if (paddle1.Y > PLAYER_MAX.BOTTOM) {
+    paddle1.Y = PLAYER_MAX.BOTTOM;
+    paddle1.speedY = 0;
+  }
+  else if (paddle2.Y < PLAYER_MAX.TOP) {
+    paddle2.Y = PLAYER_MAX.TOP;
+    paddle2.speedY = 0;
+  }
+  else if (paddle2.Y > PLAYER_MAX.BOTTOM) {
+    paddle2.Y = PLAYER_MAX.BOTTOM;
+    paddle2.speedY = 0;
   }
 }
+function detectPaddleBounce(ball,paddle1,paddle2) {
+
+  paddle1.leftX = paddle1.x;
+  paddle1.topY = paddle1.y;
+  paddle1.rightX = paddle1.x + PLAYER_WIDTH;
+  paddle1.downY = paddle1.y + PLAYER_HEIGHT;
+
+  paddle2.leftX = paddle2.x;
+  paddle2.topY = paddle2.y;
+  paddle2.rightX = paddle2.x + PLAYER_WIDTH;
+  paddle2.downY = paddle2.y + PLAYER_HEIGHT;
+  
+
+  ball.leftX =  ball.x;
+  ball.topY =  ball.y;
+  ball.rightX = ball.x + BALL_SIZE;
+  ball.downY =  ball.y + BALL_SIZE;
+
+  // TODO: Return true if they are overlapping, false otherwise
+  if ((paddle1.rightX > ball.leftX)&&
+    (paddle1.leftX < ball.rightX)&&
+    (paddle1.topY < ball.downY)&&
+    (paddle1.downY > ball.topY)&&
+    (paddle2.rightX < ball.leftX)&&
+    (paddle2.leftX > ball.rightX)&&
+    (paddle2.topY > ball.downY)&&
+    (paddle2.downY < ball.topY)) {
+    
+      ball.speedX *= -1 
+  }
+else {
+  return false;
+}
+  
+}
+
 
   /* 
   On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
@@ -154,14 +202,14 @@ function detectBounce(ball,paddle1,paddle2) {
     repositionGameItem2()
     repositionBall()
     moveBall(ball)
-    detectBounce(ball,paddle1,paddle2)
+    detectWallBounce(ball)
+    detectPaddleBounce(ball,paddle1,paddle2)
   }
   
   /* 
   Called in response to events.
   */
-  
-
+ 
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
